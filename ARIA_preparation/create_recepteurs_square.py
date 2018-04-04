0d52a9b44725e4f6c79e381b6186530c025b71d0
gitpdg <31730281+gitpdg@users.noreply.github.com>
2018-04-04 11:25:54 +0200

    Update create_recepteurs_square.py

--- ARIA_preparation/create_recepteurs_square.py
+++ ARIA_preparation/create_recepteurs_square.py
@@ -1,13 +1,16 @@
 #author : Paul
-#Creates a 10x10 grid of receptors (with ARIA format) to be used for the square
+#Creates a 10x10 grid of receptors (with ARIA format) to be used for Rouen
 
-f = open("C:/Users/adminuser/Desktop/recepteurs/recepteurs.csv","w")
+f = open("Documents/X/2A/PSC/exemples de formats/recepteurs.csv","w")
 f.write("R_INDEX;R_NAME;R_X;R_Y;R_ALTI\n")
 id=1
-for i in range(10):
-    R_X = round(-0.9+i*0.2,1)
-    for j in range(10):
-        R_Y = round(-0.9+j*0.2,1)
+x_min,x_max,y_min,y_max = (354993.3893242981, 367619.6486670857, 5470383.826645574, 5483701.332441034)
+division = 10
+
+for i in range(division):
+    R_X = round(x_min + (x_max-x_min)*(1/2+i),1)
+    for j in range(division):
+        R_Y = round(y_min + (y_max-y_min)*(1/2+j),1)
         f.write(str(id) +";"+str(int(R_X*10))+"_"+str(int(R_Y*10))+";"+str(R_X)+";"+str(R_Y)+";0\n")
         id += 1
 
